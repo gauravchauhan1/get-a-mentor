96c0cfb7bf0bc434098a1e2a887342ae82667a03
ashish-ad <73051572+ashish-ad@users.noreply.github.com>
2022-06-14 08:19:27 +0530

    ver 9: fixed some bugs in the backend

--- Backend/server.js
+++ Backend/server.js
@@ -5,22 +5,22 @@ import dotnet from "dotenv";
 dotnet.config();
 const PORT = process.env.PORT || 5000;
 
-// process.on("uncaughtException", err => {
-//   console.log("UNCAUGHT EXCEPTION!! SHUTTING DOWN...");
-//   console.log(err.name, err.message);
-//   process.exit(1);
-// });
+process.on("uncaughtException", err => {
+  console.log("UNCAUGHT EXCEPTION!! SHUTTING DOWN...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
 const server = app.listen(PORT, () => {
   console.log(`server running on port http://localhost:${PORT}`);
 });
 
-// process.on("unhandledRejection", err => {
-//   console.log("UNHANDLED REJECTION!! SHUTTING DOWN...");
-//   console.log(err.name, err.message);
-//   server.close(() => {
-//     process.exit(1);
-//   });
-// });
+process.on("unhandledRejection", err => {
+  console.log("UNHANDLED REJECTION!! SHUTTING DOWN...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
 
 mongooseConnection();
